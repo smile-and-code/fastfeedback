3541e29b36deab1bb12374bc93369bf5865b1442
sandra <samalvarez.dev@gmail.com>
2021-08-18 13:06:34 -0600

    setup chakra ui, absolute imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { AuthProvider } from '@/lib/auth';
-import '../styles/globals.css';
+import theme from '@/styles/theme';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
